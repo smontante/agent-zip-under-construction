b98b319a17919936e1f50bf2530bfbaffdfe13de
smontante <shaunmontante@gmail.com>
2019-07-01 18:47:28 -0700

    changed server config

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('../db/mongoose')
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const Ticket = require('../models/ticket')
 const publicDirectioryPath = path.join(__dirname, '../public');
 
